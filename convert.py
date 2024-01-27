2471362723f046dc4b35595d141f3ddbce90a7b4
Nigel <nigel@synergitech.com.au>
2024-01-27 14:03:23 +1100

    Added user and year variables to the jinja template for title and headings. Values are extraced from the input filename which is expected to be formatted accordingly.
    Fixes #1

--- convert.py
+++ convert.py
@@ -1,24 +1,23 @@
 from jinja2 import Environment, FileSystemLoader
 import json
 import re
-import os
 
 def makeHTML(in_file):
     # Load template
     env = Environment(loader=FileSystemLoader('html/templates'))
     index_template = env.get_template('index.jinja')
 
+    # Configure filename html parameters
+    file_pattern = r"/([a-z0-9_]*)\."
+    filename = re.search(file_pattern, in_file)[1]
+    year = re.search(r'(\d+$)', filename)[1]
+    user = re.search(r'^([a-zA-Z0-9]*)', filename)[1]
+
     # Read json file, covert to html
     f = open(in_file, 'r')
     posts = json.load(f)
-    output = index_template.render(posts=posts)
+    output = index_template.render(posts=posts, year=year, user=user)
     f.close()
-
-    # Configure filename
-    # in_file format    f'output/{user}_posts_{current_year}.json'
-    file_pattern = r"/([a-z0-9_]*)\."
-    filename = re.search(file_pattern, in_file)[1]
-    # filename = in_file[:in_file.rfind('.')]
 
     # if f'{filename}.html' not in [f for f in os.listdir('html/output')]:
     with open(f'html/output/{filename}.html', 'w') as page:
